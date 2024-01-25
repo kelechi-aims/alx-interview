a2ed06c6a7916e9ed933878659d95c8df1b2d2a9
kelechi-aims <kelechi@aims.edu.gh>
2024-01-26 00:27:46 +0100

    reads stdin line by line and computes metrics

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,36 +1,42 @@
 #!/usr/bin/python3
-"""
-A script that reads stdin line by line and computes metrics
-"""
+"""Python script that reads stdin line by line and computes metrics"""
+
 import sys
 
 
-total_size = 0
-status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-line_count = 0
+def print_n(t_file_size, status):
+    """Prints total file size and status list"""
+    print("File size: {:d}".format(t_file_size))
+    for key, value in sorted(status.items()):
+        if value != 0:
+            print("{}: {}".format(key, value))
 
+
+status = {'200': 0, '301': 0, '400': 0, '401': 0,
+          '403': 0, '404': 0, '405': 0, '500': 0}
+
+t_file_size = 0
+count = 0
 try:
     for line in sys.stdin:
-        parts = line.split()
-        if len(parts) > 2:
-            size = int(parts[-1])
-            status = int(parts[-2])
-            total_size += size
-            if status in status_codes:
-                status_codes[status] += 1
+        args = line.split()
 
-            line_count += 1
-            if line_count % 10 == 0:
-                print(f"File size: {total_size}")
-                for code in sorted(status_codes.keys()):
-                    if status_codes[code] > 0:
-                        print(f"{code}: {status_codes[code]}")
-                print()
+        if len(args) > 2:
+            status_code = args[-2]
+            file_size = int(args[-1])
+
+            if status_code in status:
+                status[status_code] += 1
+
+            t_file_size += file_size
+            count += 1
+
+            if count == 10:
+                print_n(t_file_size, status)
+                count = 0
 
 except KeyboardInterrupt:
     pass
+
 finally:
-    print(f"File size: {total_size}")
-    for code in sorted(status_codes.keys()):
-        if status_codes[code] > 0:
-            print(f"{code}: {status_codes[code]}")
+    print_n(t_file_size, status)
